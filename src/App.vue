f17e890fb287195ada624fe43271a0f838032c1c
Alessia9802 <alessiamisciagna@yahoo.it>
2021-11-18 15:21:40 +0100

    Axios doesn't work

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,74 @@
 <template>
   <div id="app">
-    <Siteheader />
+    <div id="site_header">
+      <!-- navigation bar -->
+      <nav>
+        <div class="search_box">
+          <input type="text" placeholder="search" />
+          <button>Cerca</button>
+        </div>
+      </nav>
+    </div>
     <!-- site_header -->
-    <Sitemain />
+    <div id="site_main">
+      <div class="movie" v-for="movie in movies" :key="movie.id">
+        <!-- title -->
+        <div class="title">
+          {{ movie.title }}
+        </div>
+        <!-- original_title -->
+        <div class="original_title">
+          {{ movie.original_title }}
+        </div>
+        <!-- language -->
+        <div class="original_language">
+          {{ movie.original_language }}
+        </div>
+        <!-- Vote -->
+        <div class="vote_average">
+          {{ movie.vote_average }}
+        </div>
+      </div>
+    </div>
     <!-- site_main -->
-    <Sitefooter />
+    <div id="site_footer"></div>
     <!-- site_footer -->
   </div>
 </template>
 
 <script>
-import Siteheader from "./components/Siteheader.vue";
-import Sitemain from "./components/Sitemain.vue";
-import Sitefooter from "./components/Sitefooter.vue";
+import axios from "axios";
+//import Siteheader from "./components/Siteheader.vue";
+//import Sitemain from "./components/Sitemain.vue";
+//import Sitefooter from "./components/Sitefooter.vue";
 
 export default {
   name: "App",
-  components: {
-    Siteheader,
-    Sitemain,
-    Sitefooter,
+  data() {
+    return {
+      movies: [],
+    };
   },
+  mounted() {
+    axios
+      .get(
+        "https://api.themoviedb.org/3/search/movie?api_key=9753c357316b77220b9816dfee198cf6&language=it-IT&query=it&page=1"
+      )
+      .then((r) => {
+        this.movies = r.data.response;
+        console.log(r.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  },
+  components: {},
 };
 </script>
 
 <style lang="scss">
 @import "./assets/scss/common.scss";
+.search_box {
+  padding: 1rem;
+}
 </style>
--- src/components/Sitemain.vue
+++ src/components/Sitemain.vue
@@ -5,25 +5,7 @@
 
 <script>
 //import axios from "axios";
-export default {
-  mounted() {
-    var axios = require("axios");
-
-    var config = {
-      method: "get",
-      url: "https://api.themoviedb.org/3/search/movie?api_key=9753c357316b77220b9816dfee198cf6&language=it-IT&query=it&page=1&include_adult=false",
-      headers: {},
-    };
-
-    axios(config)
-      .then(function (response) {
-        console.log(JSON.stringify(response.data));
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-  },
-};
+export default {};
 </script>
 
 <style lang="scss"></style>
