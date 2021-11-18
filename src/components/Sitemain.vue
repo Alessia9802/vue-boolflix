2b97b1322b0c4e528d62b56b224a78ce7548e466
Alessia9802 <alessiamisciagna@yahoo.it>
2021-11-18 12:18:39 +0100

    Add axios call API

--- src/components/Sitemain.vue
+++ src/components/Sitemain.vue
@@ -4,7 +4,26 @@
 </template>
 
 <script>
-export default {};
+//import axios from "axios";
+export default {
+  mounted() {
+    var axios = require("axios");
+
+    var config = {
+      method: "get",
+      url: "https://api.themoviedb.org/3/search/movie?api_key=9753c357316b77220b9816dfee198cf6&language=it-IT&query=it&page=1&include_adult=false",
+      headers: {},
+    };
+
+    axios(config)
+      .then(function (response) {
+        console.log(JSON.stringify(response.data));
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  },
+};
 </script>
 
 <style lang="scss"></style>
