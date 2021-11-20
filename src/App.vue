1267cd7ed170cde2ef85dddcf82b4247fd8301e8
Alessia9802 <alessiamisciagna@yahoo.it>
2021-11-20 17:36:15 +0100

    Milestone 1

--- src/App.vue
+++ src/App.vue
@@ -4,29 +4,35 @@
       <!-- navigation bar -->
       <nav>
         <div class="search_box">
-          <input type="text" placeholder="search" />
-          <button>Cerca</button>
+          <input
+            type="text"
+            placeholder="cerca un film"
+            v-model="searchString"
+          />
+          <button @click="callApi">Cerca</button>
         </div>
       </nav>
     </div>
     <!-- site_header -->
     <div id="site_main">
-      <div class="movie" v-for="movie in movies" :key="movie.id">
-        <!-- title -->
-        <div class="title">
-          {{ movie.title }}
-        </div>
-        <!-- original_title -->
-        <div class="original_title">
-          {{ movie.original_title }}
-        </div>
-        <!-- language -->
-        <div class="original_language">
-          {{ movie.original_language }}
-        </div>
-        <!-- Vote -->
-        <div class="vote_average">
-          {{ movie.vote_average }}
+      <div class="movies">
+        <div class="movie" v-for="movie in movies" :key="movie.id">
+          <!-- title -->
+          <div class="title">
+            <h1>{{ movie.title }}</h1>
+          </div>
+          <!-- original_title -->
+          <div class="original_title">
+            {{ movie.original_title }}
+          </div>
+          <!-- language -->
+          <div class="original_language">
+            {{ movie.original_language }}
+          </div>
+          <!-- Vote -->
+          <div class="vote_average">
+            {{ movie.vote_average }}
+          </div>
         </div>
       </div>
     </div>
@@ -46,21 +52,31 @@ export default {
   name: "App",
   data() {
     return {
-      movies: [],
+      searchString: "",
+      movies_url: "https://api.themoviedb.org/3/search/movie",
+      api_key: "9753c357316b77220b9816dfee198cf6",
+      error: null,
+      movies: "",
     };
   },
-  mounted() {
-    axios
-      .get(
-        "https://api.themoviedb.org/3/search/movie?api_key=9753c357316b77220b9816dfee198cf6&language=it-IT&query=it&page=1"
-      )
-      .then((r) => {
-        this.movies = r.data.response;
-        console.log(r.data);
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
+  methods: {
+    callApi() {
+      console.log("cliccato");
+      console.log(
+        `${this.movies_url}?api_key=${this.api_key}&query=${this.searchString} `
+      );
+      const full_url = `${this.movies_url}?api_key=${this.api_key}&query=${this.searchString} `;
+      axios
+        .get(full_url)
+        .then((resp) => {
+          console.log(resp);
+          this.movies = resp.data.results;
+        })
+        .catch((error) => {
+          console.log(error);
+          this.error = error;
+        });
+    },
   },
   components: {},
 };
